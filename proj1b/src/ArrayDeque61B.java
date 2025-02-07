36ff5f8be136c686fb03f6581739a4520ad42991
alenas <alenasred@hotmail.com>
2025-02-07 20:04:33 +0100

    get

---
+++ proj1b/src/ArrayDeque61B.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
+
+public class ArrayDeque61B<T> implements Deque61B<T>{
+   private T[] items;
+   private int size;
+   private int nextFirst;
+   private int nextLast;
+
+    public ArrayDeque61B() {
+        items = (T[]) new Object[8];
+        size = 0;
+        nextFirst = 3;
+        nextLast = 4;
+    }
+
+    //add first update next position and size
+    @Override
+    public void addFirst(T x) {
+        if(x == null)
+            return;
+        //add resize if size == items.length
+
+        items[Math.floorMod(nextFirst, items.length)] = x;
+        nextFirst--;
+        size++;
+    }
+
+    //add last update next position and size
+    @Override
+    public void addLast(T x) {
+        if(x == null)
+            return;
+
+        //add resize if size == items.length
+
+        items[Math.floorMod(nextLast, items.length)] = x;
+        nextLast++;
+        size++;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> list = new ArrayList<>(size);
+        for(int i = nextFirst + 1; i < nextLast; i++) {
+            list.add(items[Math.floorMod(i, items.length)]);// replace later with get
+        }
+        return list;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    //starting point "nextFirst + 1" + index and mod it so you can circle in array
+    @Override
+    public T get(int index) {
+        if(index < 0 || index >= size)
+            return null;
+        return items[Math.floorMod(index + nextFirst + 1, items.length)];
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -10,14 +10,67 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BTest {
 
-//     @Test
-//     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
-//     void noNonTrivialFields() {
-//         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
-//                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-//                 .toList();
-//
-//         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-//     }
+     @Test
+     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
+     void noNonTrivialFields() {
+         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
+                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                 .toList();
 
+         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+     }
+    @Test
+    void addFirstTest() {
+       ArrayDeque61B<Integer> arr1 = new ArrayDeque61B<>();
+
+       assertThat(arr1.toList()).containsExactly();
+
+        arr1.addFirst(6);
+        arr1.addFirst(5);
+        arr1.addFirst(4);
+        arr1.addFirst(3);
+        arr1.addFirst(2);
+        arr1.addFirst(1);
+
+        assertThat(arr1.toList()).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    void addLastTest() {
+        ArrayDeque61B<Integer> arr1 = new ArrayDeque61B<>();
+
+        assertThat(arr1.toList()).containsExactly();
+
+        arr1.addLast(6);
+        arr1.addLast(5);
+        arr1.addLast(4);
+        arr1.addLast(3);
+        arr1.addLast(2);
+        arr1.addLast(1);
+
+        assertThat(arr1.toList()).containsExactly(6, 5, 4, 3, 2, 1);
+    }
+    @Test
+    void addFirstLastTest() {
+        ArrayDeque61B<Integer> arr1 = new ArrayDeque61B<>();
+
+        assertThat(arr1.toList()).containsExactly();
+
+        arr1.addLast(6);
+        arr1.addFirst(5);
+        arr1.addLast(4);
+        arr1.addFirst(3);
+        arr1.addLast(2);
+        arr1.addFirst(1);
+
+        assertThat(arr1.toList()).containsExactly(1, 3, 5, 6, 4, 2);
+    }
+
+    @Test
+    void getTest() {
+         ArrayDeque61B<Integer> arr1 = new ArrayDeque61B<>();
+
+         for(int i = 1; i <= 6; i++)
+
+    }
 }
